035be0acd3bacfe3aac9ed31ca6ae58b7dbb62b3
romanman <roman.mandeleil@gmail.com>
2014-01-27 10:26:01 +0200

    New Project Take Off

---
+++ src/main/java/org.openchain/db/Block.java
@@ -0,0 +1,15 @@
+package org.openchain.db;
+
+/**
+ * www.openchain.info
+ * User: Roman Mandeleil
+ * Created on: 27/01/14 10:23
+ */
+public class Block {
+
+    String hash;
+    Long   index;
+
+    String hexRaw;
+
+}
